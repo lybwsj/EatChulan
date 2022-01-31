45b138cf19eb62f2a77921222528b954548c3050
lybwsj <98733931+lybwsj@users.noreply.github.com>
2022-01-31 17:01:51 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -441,10 +441,10 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
             SubmitResults();
         }
 
-        if (cps <= 5) return '试着好好练一下？';
-        if (cps <= 8) return 'TCL';
-        if (cps <= 10)  return 'TQL';
-        if (cps <= 15) return '您';
+        if (cps <= 5) return '菜鸡哈哈哈';
+        if (cps <= 8) return '不行啊您这';
+        if (cps <= 10)  return '您会炁体源流？';
+        if (cps <= 15) return '宝儿姐来埋人啦';
         return '人？';
     }
 
